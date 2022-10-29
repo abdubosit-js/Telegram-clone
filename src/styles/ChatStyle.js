e66500d1b0f6d54490acdbd2a91588862c2424ab
abdubosit-js <abdubositjs@gmail.com>
2022-10-29 18:23:51 +0500

    o'zgardi ikki

---
+++ src/styles/ChatStyle.js
@@ -0,0 +1,167 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+    width: 100%;
+    position: relative;
+    .top-group-name-cnt {
+        width: 100%;
+        height: 83px;
+        border-bottom: 1px solid #A1AAB3;
+        display: flex;
+        align-items: center;
+        gap: 20px;
+        padding: 0 30px;
+        .group-image {
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #10a100;
+            color: white;
+            font-size: 11px;
+            font-weight: 600;
+        }
+        .group-name {
+            h2 {
+                color: #ffffff;
+            }
+
+            p {
+                color: #A1AAB3;
+            }
+        }
+    }
+    .chat_wrapper {
+        width: 100%;
+        height: 53px;
+        background-color: transparent;
+        bottom: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        .chat {
+            width: 95%;
+            height: 53px;
+            position: relative;
+            background-color: #383442;
+            border-radius: 8px;
+            input {
+                padding: 10px 60px;
+                width: 100%;
+                min-height: 100%;
+                border: none;
+                outline: none;
+                background-color: transparent;
+            }
+            
+            input[type="text"] {
+                color: white;
+                font-size: 20px;
+            }
+            .attachment {
+                position: absolute;
+                top: 2px;
+                left: 5px;
+                transform: rotate(120deg);
+                cursor: pointer;
+                :hover {
+                    path {
+                        fill: white;
+                    }
+                }
+            }
+            .send {
+                position: absolute;
+                top: 2px;
+                right: 30px;
+                cursor: pointer;
+                path {
+                    fill: #0099ff;
+                }
+            }
+        }   
+    }
+    
+    .messages_container {
+        width: 100%;
+        height: 78vh;
+        overflow: scroll;
+        overflow-x: hidden;
+        padding: 20px 40px;
+        padding-bottom: 1px;
+        ::-webkit-scrollbar {
+            width: 10px;
+        }
+        ::-webkit-scrollbar-thumb {
+            background: #ffffff; 
+            border-radius:10px;
+        }
+        ::-webkit-scrollbar-thumb:hover {
+            background: #dbdbdb; 
+        } 
+    }
+
+    .message_wrapper {
+        position: relative;
+        display: flex;
+        gap: 10px;
+        align-items: center;
+        margin-bottom: 15px;
+
+        .profile-cnt {
+            width: 40px;
+            height: 40px;
+            color: white;
+            font-size: 10px;
+            text-transform: capitalize;
+            display: flex;
+            align-items: center;
+            margin-top: 35px;
+            justify-content: center;
+            border-radius: 50%;
+            background-color: red;
+        }
+
+        .message {
+            min-height: 70px;
+            background: #383442;
+            display: flex;
+            flex-direction: column;
+            padding: 3px 10px;
+            border-radius: 14px 14px 14px 4px;
+            position: relative;
+            p {
+                color: green;
+                font-weight: 600;
+                text-transform: capitalize;
+            }
+           
+            h4 {
+                color: white;
+                font-weight: 400;
+            }
+            
+            h6 {
+                margin-top: 6px;
+                font-style: normal;
+                font-weight: 350;
+                font-size: 12px;
+                line-height: 14px;
+                text-align: right;
+                color: #A1AAB3;
+            }
+            .delete {
+                color: white;
+                background-color: red;
+                padding: 0 5px;
+                border-radius: 50%;
+                position: absolute;
+                left: 0px;
+                bottom: 0px;
+                cursor: pointer;
+            }
+        }
+    }
+`
---
+++ src/styles/HeaderStyle.js
@@ -0,0 +1,154 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+    width: 400px;
+    height: 100vh;
+    background-color: #141120;
+    padding: 10px;
+    .group_container {
+        width: 100%;
+        height: 70px;
+        display: flex;
+        align-items: center;
+        gap: 14px;
+        padding-left: 10px;
+        background-color: ${({isActive}) => isActive ? "#383442" : "transparent"};
+        border-radius: 5px;
+        transition: 0.3s;
+        cursor: pointer;
+        .line {
+            width: 3px;
+            border-radius: 2px;
+            height: 30px;
+            transition: 0.3s;
+            background-color: ${({isActive}) => isActive ? "#60CDFF" : "transparent"};
+            margin-left: -10px;
+        }
+        h2 {
+            color: white;
+        }
+
+        p {
+            color: gray;
+        }
+        .chanel-group {
+            width: 60px;
+            height: 60px;
+            border-radius: 50%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #10a100;
+            font-weight: 600;
+            font-size: 12px;
+            color: white;
+            cursor: pointer;
+        }
+      
+    }
+    .search-cnt {
+        width: 100%;
+        height: 35px;
+        background: #383442;
+        border-radius: 5px;
+        input {
+            background: transparent;
+            width: 100%;
+            height: 100%;
+            padding-left: 20px;
+            border: none;
+            outline: none;
+        }
+        input[type="text"] {
+            color: white;
+            font-family: sans-serif;
+        }
+        margin-bottom: 15px;
+    }
+
+    .menu-div{
+        width: 60px;
+        height: 60px;
+        .menu-cnt {
+            max-width: 100%;
+            max-height: 100%;
+            cursor: pointer;
+            :hover {
+                path {
+                    fill: white;
+                }
+            }
+        }
+    }
+`
+
+export const MenuCnt = styled.div`
+    z-index: 10;
+    width: 300px;
+    height: 100vh;
+    background-color: #141120;
+    position: absolute;
+    transition: 0.3s;
+    left: ${({active}) => active ? "0px" : "-300px"};
+    display: flex;
+    flex-direction: column;
+    .signup_cnt {
+        width: 100%;
+        height: 40px;
+        display: flex;
+        align-items: center;
+        padding-left: 15px;
+        border-top: 1px solid gray;
+        border-bottom: 1px solid gray;
+        :hover {
+            background-color: #383442;
+        }
+        button {
+            background-color: transparent;
+            color: white;
+            border: none;
+            font-size: 20px;
+            cursor: pointer;
+        }
+        cursor: pointer;
+    }
+    .signin-cnt {
+        width: 100%;
+        height: 40px;
+        display: flex;
+        align-items: center;
+        padding-left: 15px;
+        border-bottom: 1px solid gray;
+    }
+    .profile-cnt {
+        width: 100%;
+        height: 130px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        .avatar-cnt {
+            width: 60px;
+            height: 60px;
+            color: white;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 22px;
+            font-weight: 600;
+            background-color: green;
+            border-radius: 50%;
+        }
+    }
+    a {
+        color: white;
+        text-decoration: none;
+    }
+`
+
+export const Opasity = styled.div`
+    position: absolute;
+    z-index: 1;
+    width: 100%;
+    height: 100vh;
+    background-color: #00000090;
+`
